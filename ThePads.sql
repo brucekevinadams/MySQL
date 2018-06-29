fbfae23e032f94f72dba2d542e982f91073676e1
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-06-29 11:28:25 -0500

    Update ThePads.sql

--- ThePads.sql
+++ ThePads.sql
@@ -1,3 +1,10 @@
+/*
+ * Author: Bruce Adams
+ * email: ezaroth@gmail.com
+ * website: austingamestudios.com
+ * 
+*/
+
 SELECT concat(NAME, CASE WHEN occupation = "Doctor" THEN "(D)" WHEN occupation = "Professor" THEN
 "(P)" WHEN occupation = "Singer" THEN "(S)" WHEN occupation = "Actor" THEN "(A)" END ) FROM
 OCCUPATIONS ORDER BY NAME; SELECT "There are a total of ", COUNT(OCCUPATION),
