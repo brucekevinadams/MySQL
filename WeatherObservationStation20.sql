9ef6e1d79bdbd3540e6cc10186c978b56a728738
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-06-29 11:29:54 -0500

    Update WeatherObservationStation20.sql

--- WeatherObservationStation20.sql
+++ WeatherObservationStation20.sql
@@ -1,2 +1,9 @@
+/*
+ * Author: Bruce Adams
+ * email: ezaroth@gmail.com
+ * website: austingamestudios.com
+ * 
+*/
+
 Select round(S.LAT_N,4) mediam from station S where (select count(Lat_N) from station 
 where Lat_N < S.LAT_N ) = (select count(Lat_N) from station where Lat_N > S.LAT_N)
