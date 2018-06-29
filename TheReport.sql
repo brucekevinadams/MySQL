b7646ed7f928da1c283f0170a52353f14a6397d8
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-06-29 11:28:48 -0500

    Update TheReport.sql

--- TheReport.sql
+++ TheReport.sql
@@ -1,3 +1,10 @@
+/*
+ * Author: Bruce Adams
+ * email: ezaroth@gmail.com
+ * website: austingamestudios.com
+ * 
+*/
+
 SELECT (CASE g.grade>=8 WHEN TRUE THEN s.name ELSE null END),g.grade,s.marks 
 FROM students s INNER JOIN grades g ON s.marks BETWEEN min_mark AND max_mark 
 ORDER BY g.grade DESC,s.name,s.marks;
