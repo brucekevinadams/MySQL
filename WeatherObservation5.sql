b4d3cd872fa209925c1b734cd3c71f9d51556428
Bruce Adams <22304833+brucekevinadams@users.noreply.github.com>
2018-06-29 11:29:30 -0500

    Update WeatherObservation5.sql

--- WeatherObservation5.sql
+++ WeatherObservation5.sql
@@ -1,2 +1,9 @@
+/*
+ * Author: Bruce Adams
+ * email: ezaroth@gmail.com
+ * website: austingamestudios.com
+ * 
+*/
+
 select CITY, length(CITY)from station order by length(CITY), city limit 1; select CITY, 
 length(CITY)from station order by length(CITY) desc, city limit 1;
